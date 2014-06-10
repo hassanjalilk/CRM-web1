70e66016b08f9eac74c70324293c9a434c0e28a5
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-10 17:05:55 -0400

    CRM-Web

---
+++ CRM-web.rb
@@ -0,0 +1,54 @@
+
+require_relative 'rolodex'
+require_relative 'contact'
+require 'sinatra'
+
+@@rolodex = Rolodex.new
+
+get '/' do 
+	@crm_app_name = "My CRM"
+	erb :index
+end
+
+get '/contacts' do
+	erb :contact
+end
+
+get '/contacts/new' do
+	erb :new_contact
+end
+
+get '/contacts' do
+	puts params
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/')
+end
+
+
+
+
+
+
+# require 'sinatra'
+# require_relative 'contact'
+
+# get '/' do
+# 	@crm_app_name = "My CRM"
+# 	erb :index
+# end
+
+# #view all contacts
+# get "/contacts" do
+#   @contacts = []
+#   @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+#   @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+#   @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+#   erb :contacts
+# end
+
+ 
---
+++ rolodex.rb
@@ -0,0 +1,14 @@
+class Rolodex
+  attr_reader :contacts
+
+  def initialize
+    @contacts = []
+    @contact_id = 1000
+  end
+
+  def add_contact(contact)
+    contact.id = @contact_id
+    @contacts << contact
+    @contact_id += 1
+  end
+end

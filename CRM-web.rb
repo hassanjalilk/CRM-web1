ff8680fc0e15ea3ffcd4db41206ec342768b12cf
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-12 15:10:37 -0400

    Editing Contact

--- CRM-web.rb
+++ CRM-web.rb
@@ -5,6 +5,14 @@ require 'sinatra'
 
 @@rolodex = Rolodex.new
 @@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 
 get '/' do 
 	erb :index
@@ -32,6 +40,34 @@ get "/contacts/1000" do
 	@contact = @@rolodex.find(1000)
 	erb :show_contact
 end
+
+get "/contacts/1000" do
+	@contacts = @@rolodex.find(params[:id].to_i)
+	erb :show_contact
+end
+
+get "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  erb :edit_contact
+end
+
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 
 
 
